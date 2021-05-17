57a6b12f059d7975093ec760a9c548688f572639
jeffreychan <kkjeffreychan@gmail.com>
2021-05-16 23:29:25 -0400

    first commit

---
+++ server/Index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const { Console } = require("console");
+
+//specify port
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+//initialize express
+const app = express();
+const server = http.createServer(app);
+// instance of socketio
+const io = socketio(server);
+
+//passing middleware router component
+app.use(router);
+
+//runs server
+//1st param is port
+//2nd function
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
+//real time messaging
+//create simple router as new component
