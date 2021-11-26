5517b27215d8dc499d28c2008f7b19a62fd95295
jeffreychan <kkjeffreychan@gmail.com>
2021-11-25 19:07:39 -0500

    fixing dependabot messages

--- Scribble/client/src/components/Drawing/Drawing.js
+++ Scribble/client/src/components/Drawing/Drawing.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Line } from "react-lineto";
 
 const useMousePosition = () => {
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
@@ -14,9 +15,10 @@ const useMousePosition = () => {
 
 const Drawing = () => {
   const [holding, setHolding] = useState(false);
-  const [cooardinate, setcooardinate] = useState({ x: 0, y: 0 });
+  const [cooardinate, setcooardinate] = useState({ x: null, y: null });
   const [cooardinates, setCooardinates] = useState([]);
-  const delay = 1;
+  const [allLines, setAllLines] = useState([]);
+
   const { x, y } = useMousePosition();
 
   const addToArray = () => {
@@ -29,8 +31,10 @@ const Drawing = () => {
 
   useEffect(() => {
     if (holding) {
-      let timer1 = setTimeout(() => setcooardinate({ x: x, y: y }), delay * 10);
-      addToArray();
+      let timer1 = setTimeout(() => {
+        setcooardinate({ x: x, y: y });
+        addToArray();
+      }, 10);
       return () => {
         clearTimeout(timer1);
       };
@@ -42,6 +46,31 @@ const Drawing = () => {
       onMouseDown={() => setHolding((e) => !e)}
       onMouseUp={() => setHolding(false)}
     >
+      {cooardinates.map((element, idx, array) => {
+        console.log("current", element);
+        console.log("next", array[idx + 1]);
+        if (array[idx + 1]) {
+          return (
+            <Line
+              x0={element[0]}
+              y0={element[1]}
+              x1={array[idx + 1][0]}
+              y1={array[idx + 1][1]}
+              borderColor="black"
+              borderWidth="60"
+            />
+          );
+        } else {
+          return;
+        }
+      })}
+      {/* <Line
+        x0={0}
+        y0={0}
+        x1={100}
+        y1={100}
+        
+      /> */}
       <p>
         {x} and {y}
       </p>
